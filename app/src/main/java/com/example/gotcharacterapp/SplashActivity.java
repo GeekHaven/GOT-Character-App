9d2c6365e8a5049ce4a55e9730e7e7e6c1701b88
Rahul Dev <iit2019053@iiita.ac.in>
2020-10-11 22:06:36 +0530

    modified splash screen background

---
+++ app/src/main/java/com/example/gotcharacterapp/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.gotcharacterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        },3000);
+    }
+}
