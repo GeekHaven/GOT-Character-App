1f5f90e681803083268eb4528236bb79f72ea08b
Mandar16 <iit2019059@iiita.ac.in>
2020-10-13 12:36:45 +0530

    Added searchView in toolbar

--- app/src/main/java/com/example/gotcharacterapp/MainActivity.java
+++ app/src/main/java/com/example/gotcharacterapp/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.gotcharacterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,5 +43,27 @@ public class MainActivity extends AppCompatActivity {
         //Using recycler view
         RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(MainActivity.this,generalList);
         recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search,menu);
+        MenuItem searchViewItem = menu.findItem(R.id.app_bar_search);
+
+        SearchView searchView = (SearchView)searchViewItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
